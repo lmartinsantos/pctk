8507c13d6c34faf57b09503903b474630715a631
Javier Solana <javier.solana@cabify.com>
2024-09-22 01:55:51 +0200

    remove comment

--- examples/scene/main.go
+++ examples/scene/main.go
@@ -10,17 +10,4 @@ func main() {
 	app := pctk.New(loader)
 	app.Do(pctk.ScriptRun{ScriptResource: pctk.NewResourceRef("resources", "scripts/LostMyKeys")})
 	app.Run()
-	/*
-
-		objects_sprites := pctk.LoadSpriteSheetFromFile("items.png", pctk.Size{W: 30, H: 18})
-			bundle.PutSpriteSheet("/objects/sprites", objects_sprites)
-
-			bucket := pctk.NewObject("bucket").WithAnimation(pctk.NewAnimation("/objects/sprites").
-				WithFrame(5, 6, 0*time.Second)).WithScript(pctk.LookAt, &pctk.Script{
-				Language: pctk.ScriptLua,
-				Code: []byte(`
-					ActorSpeak("guybrush", "Mmmm, nice bucket!", {delay=1000}).Wait()
-				`)})
-			bundle.PutObject("/objects/bucket", bucket)
-	*/
 }
